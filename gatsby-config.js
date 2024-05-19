c54cdb86c75d51b3997b0d430a7fe149e4da019e
louddays <team@louddays.com>
2024-05-19 18:12:59 +0530

    Add required ext

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  DEV_SSR: false,
   siteMetadata: {
     title: `Watermen Plumbing`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
