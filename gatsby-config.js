50dec98f0abd3130bd6ec8b58cb4f2db93b67dd0
louddays <team@louddays.com>
2024-05-19 17:26:26 +0530

    Add required ext

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
+// require('dotenv').config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// })
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
@@ -16,12 +13,14 @@ module.exports = {
   },
  
   plugins: [
-    {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-        enableIdentityWidget: true,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-source-prismic',
+    //   options: {
+    //     repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+    //     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    //     customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN
+    //   },
+    // },
       `gatsby-plugin-image`,
       `gatsby-plugin-sharp`,
       `gatsby-transformer-sharp`,
