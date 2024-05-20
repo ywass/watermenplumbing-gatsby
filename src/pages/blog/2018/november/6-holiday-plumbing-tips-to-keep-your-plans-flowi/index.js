80101fda197b5ea27957decdf11afc7b0991296e
louddays <team@louddays.com>
2024-05-20 12:20:08 +0530

    Add Blog pages

--- src/pages/blog/2018/november/6-holiday-plumbing-tips-to-keep-your-plans-flowi/index.js
+++ src/pages/blog/2018/november/6-holiday-plumbing-tips-to-keep-your-plans-flowi/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { graphql } from "gatsby"
+import Seo from "../../../../../components/seo"
 import Layout from '../../../../../components/layout'
 import Nov18FirstBlog from '../../../../../components/BlogPages/2018Pages/November2018Pages/Nov18FirstBlog/Nov18FirstBlog'
 function index() {
@@ -10,3 +12,28 @@ function index() {
 }
 
 export default index
+
+export const Head = (props) => {
+  let socialImage =  '';
+  if(props.data.PageData.data.seo_social_image.url){
+    const socialImageNew = props.data.PageData.data.seo_social_image.url.split('?');
+    socialImage = socialImageNew[0]
+  }  
+  return(
+    <Seo title={props.data.PageData.data.seo_title} description={props.data.PageData.data.seo_description} socialImage={socialImage} />
+  )
+}
+
+export const query = graphql`
+query BlogPage {
+  PageData : prismicPosts(uid: {eq: "6-holiday-plumbing-tips-to-keep-your-plans-flowi"}) {
+    data {
+      seo_title
+      seo_description
+      seo_social_image {
+        url
+      }
+    }
+  }
+}
+`
