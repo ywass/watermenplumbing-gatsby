3c7aa749d99ccb53f06514e6334026a1f1b1718b
louddays <team@louddays.com>
2024-05-20 13:05:52 +0530

    Revert oldversion

--- src/pages/blog/categories/septic-tanks/index.js
+++ src/pages/blog/categories/septic-tanks/index.js
@@ -12,28 +12,3 @@ function index() {
 }
 
 export default index
-
-export const Head = (props) => {
-  let socialImage =  '';
-  if(props?.data?.PageData?.data?.seo_social_image?.url){
-    const socialImageNew = props?.data?.PageData?.data?.seo_social_image?.url.split('?');
-    socialImage = socialImageNew && socialImageNew[0]
-  }  
-  return(
-    <Seo title={props.data.PageData.data.seo_title} description={props.data.PageData.data.seo_description} socialImage={socialImage ? socialImage : ''} />
-  )
-}
-
-export const query = graphql`
-query BlogPage {
-  PageData : prismicPosts(uid: {eq: "septic-tanks"}) {
-    data {
-      seo_title
-      seo_description
-      seo_social_image {
-        url
-      }
-    }
-  }
-}
-`
