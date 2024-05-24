73cc112670b9c812b4b8b42db2065931e50a92f0
louddays <team@louddays.com>
2024-05-24 05:57:54 +0530

    Add Google Tag

---
+++ src/html.js
@@ -0,0 +1,42 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Script } from "gatsby"
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
+
+        {props.headComponents}
+        <Script id="gtag-id" src="https://www.googletagmanager.com/gtag/js?id=G-83CCXZ2TZ6" strategy="off-main-thread"/>
+        <Script id="gtag-config" strategy="off-main-thread" forward={[`gtag`]}>
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-83CCXZ2TZ6');`}
+      </Script>      
+      </head>
+      <body {...props.bodyAttributes}>
+     {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}        
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
